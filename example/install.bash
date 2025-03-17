8b2de79f1a8a557e6ead2239389c77b4d8d0d5cd
Jeff Turmelle <jefft@iri.columbia.edu>
2025-03-17 16:04:05 -0400

    Address git issues and bugs in the install.bash file Modified Vagrantfile to bootstrap centos9 before resizing disk because resizedisk reboots Updated run_tests to use the ansible.cfg file for the playbook. updated ntp servers

--- example/install.bash
+++ example/install.bash
@@ -8,24 +8,32 @@
 ##
 VENV=/opt/datalib_venv3.12
 BUILD=""
-CHECK="-diff"
+CHECK=""
+FETCH=1
 
 function showhelp()
 {
   echo "$0 [--build] [--check]"
-  echo " --check:    Optional argument to check what will be done without making changes."
-  echo " --build:    Optional argument to build or rebuild the maprooms.  This can take a long time."
+  echo " --check:     Optional argument to check what will be done without making changes."
+  echo " --build:     Optional argument to build or rebuild the maprooms.  This can take a long time."
+  echo " --no-fetch:  Optional argument in case of a lack of internet connection. This will disable"
+  echo "              the fetch from the remote git repository.  This should only be used if you are"
+  echo "              completely sure that the remote repository hasn't changed, otherwise you could"
+  echo "              end up reverting to an older version without features that were previously installed."
   echo ""
   exit
 }
 
 if [ $# -gt 0 ]; then
   case $1 in
-    --build|-build|build)
+    --build)
       BUILD="-e run_update_script=yes"
       ;;
-    --check|-check|check)
-      CHECK="--check --diff"
+    --check)
+      CHECK="--check"
+      ;;
+    --no-fetch)
+      FETCH=0
       ;;
     *)
       showhelp
@@ -57,22 +65,48 @@ fi
 
 # Check for git changes
 echo "Checking for git changes"
-git fetch
-git status
+if [[ ${FETCH} == 1 ]]; then
+  git fetch
+  if [ $? != 0 ]; then
+    echo ""
+    echo "git fetch has failed. We can not communicate with the GIT cloud repository.  There are"
+    echo "several reasons this might happen: either you don't have an internet connection, or you"
+    echo "don't have permission to access the repository.  Please insure that your account has access to the"
+    echo "repository [ See: https://dldocs.iri.columbia.edu/admin/git.html ].  If your internet connection is offline"
+    echo "and this is an urgent update, then you can rerun this command with the --no-fetch command.  This should"
+    echo "only be done if you are positive that there are no changes on the server because this may revert the DL to"
+    echo "an older version without features that were previously installed.  If you can wait, you should wait."
+    echo ""
+    exit 1
+  fi
+fi
+
+gitstatus="$(git status)"
 echo ""
-echo "If your branch is behind the master repository, you should pull your changes first."
-echo "If you have changes, you should commit them when after you have insured they work."
+
+behind="branch is behind \'origin"
+if [[ "$gitstatus" =~ "$behind" ]]; then
+  echo "** Your branch is behind the cloud repository. You must pull your changes first"
+  echo "to insure you do not damage the Data Library."
+  echo ""
+  echo "Use: git pull --ff-only"
+  echo ""
+  exit 1
+fi
+
+echo ""
+echo "If you have changes, you should commit them."
 echo ""
 echo "For more information, review https://dldocs.iri.columbia.edu/admin/maintenance.html"
 echo ""
 
-if [[ "${CHECK}" == "" ]]; then
+if [[ "${CHECK}" != "" ]]; then
   echo "You are running the command in check mode, no changes will be made."
 else
-  echo "You are about to make install your changes to the Data Library installation."
+  echo "You are about to apply changes to the Data Library installation."
 fi
 echo ""
-read -p "Do you want to continue: (yes or no) (default is no): " answer
+read -p "Do you want to continue: (yes or no): " answer
 if [[ "${answer}" != "yes" ]]; then
   exit 1
 fi
